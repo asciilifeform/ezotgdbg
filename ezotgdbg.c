3bf7d8d38dfa966894398f0c97387523c9f1928c
Stanislav Datskovskiy <stas@loper-os.org>
2012-01-15 10:22:13 -0500

    ...

--- ezotgdbg.c
+++ ezotgdbg.c
@@ -4,8 +4,8 @@
 #define VENDOR_ID    0x04B4
 #define PRODUCT_ID   0x7200
 
-#define BUFSIZE      10000
-#define TIMEOUT      5000
+#define BUFSIZE      32767
+#define TIMEOUT      50000
 
 
 void save_buf(char *buf, int length, char *filename) {
@@ -60,10 +60,10 @@ int main(int argc, char *argv[]) {
   }
 
   if (current_device == NULL) {
-    printf("Could not find a CY7C68013!\n");
+    printf("Could not find a CY7C67300!\n");
     exit(1);
   } else {
-    printf("Found CY7C68013.\n");
+    printf("Found CY7C67300.\n");
   }
 
   current_handle = usb_open(current_device);
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
   printf("ID='%s'\n", buf);
   
   res = usb_control_msg(current_handle, USB_ENDPOINT_IN | USB_TYPE_VENDOR | USB_RECIP_DEVICE,
-			0xFF, 0x0007, 0x03e8, buf, 2048, TIMEOUT); // 0 was 0xfff8
+			0xFF, 0x0007, 0x1000, buf, 4096, TIMEOUT);
 
   if (res < 0) {
     printf("Error reading splat!\n");
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]) {
 
   printf("read bytes='%d'\n", res);
 
-  save_buf(buf, 2048, dumpfilename);
+  save_buf(buf, 4096, dumpfilename);
 
   usb_release_interface(current_handle, 0);
   usb_close(current_handle);
